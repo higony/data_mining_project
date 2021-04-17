0eb7e8079152e9d1533f4e05beb3a9e7ae75abab
higony <jenissjhk@likelion.org>
2021-04-17 16:52:47 +0900

    stock ver 1.0.0

---
+++ project/종목발굴/stock_price_rank.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+driver = webdriver.Chrome()
+url = "https://finance.naver.com/sise/sise_market_sum.nhn?sosok=0&page=1" # 코스피인 경우 sosok이 0, 코스닥인 경우 1. page는 몇 번째 페이지인지 나타냄.
+driver.get(url)
+
+driver.find_element_by_class_name("pgRR").click()
+pages = int(driver.current_url[-2:]) # 전체 페이지 수
+
+f = open("result.txt", "w")
+
+cnt = 0
+for sosok in range(2):
+    for page in range(1, pages+1):
+        url = f"https://finance.naver.com/sise/sise_market_sum.nhn?sosok={sosok}&page={page}"
+        driver.get(url)
+        soup = BeautifulSoup(driver.page_source, "lxml")
+        table_elements = soup.find("table", attrs={"class":"type_2"}).tbody.find_all("tr", attrs={"onmouseover":"mouseOver(this)"})
+        for element in table_elements: # 필요한 정보: 종목명, 현재가, 거래량
+            info = list([x.get_text().strip() for x in element.find_all("td")])
+            # index는 0번째, 종목명은 1번째, 현재가는 2번째, 거래량은 10번째 요소.
+
+            if info[9] == "0": # 거래량이 0이라면. 즉, 거래정지된 종목이라면.
+                continue
+            
+            cnt += 1
+            info_tuple = cnt, info[1], info[2], info[9]
+            string = f"""
+{info_tuple[0]}. {info_tuple[1]}
+{info_tuple[2]}원. 거래량: {info_tuple[3]}
+""" + "-"*100 + "\n"
+            print(string, end="")
+            f.write(string)
+
+f.close()
+driver.quit()    
