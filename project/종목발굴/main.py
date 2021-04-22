4907442035e23175d387d9291ff225e94d2170fb
higony <jenissjhk@likelion.org>
2021-04-22 15:46:30 +0900

    stock ver 1.1.4

--- project/종목발굴/main.py
+++ project/종목발굴/main.py
@@ -1,6 +1,6 @@
 # 1.1.0 : main 함수를 만들어 파일의 프로젝트화 시작
 from selenium import webdriver
-from stock_filter import stock_list_by_price, stock_list_by_amount
+from stock_filter import stock_list_by_price, stock_list_by_amount, stock_list_by_purchase
 from list_method import initialize, read_list
 import time
 
@@ -10,33 +10,40 @@ print("초기화 중입니다.")
 initialized_li = initialize(driver)
 li = initialized_li
 print("초기화를 완료했습니다.")
-try:
-    while True:
-        print("원하시는 필터 적용 및 기능을 선택해주세요.")
-        print("1. 가격별 필터")
-        # 1.1.2 : 거래량 기준 필터 기능 추가
-        print("2. 거래량 별 필터")
-        print("3. 필터 초기화")
-        print("4. 필터링 된 종목 조회")
-        print("0. 종료")
-        selection = int(input("기능 선택 >>>>>>> "))
+# try:
+while True:
+    print("원하시는 필터 적용 및 기능을 선택해주세요.")
+    print("1. 가격별 필터")
+    # 1.1.2 : 거래량 기준 필터 기능 추가
+    print("2. 거래량 별 필터")
+    # 1.1.4 : 외인 / 기관 연일 순매수 필터 기능 추가
+    print("3. 외인 순매수 별 필터")
+    print("4. 기관 순매수 별 필터")
+    print("5. 필터 초기화")
+    print("6. 필터링 된 종목 조회")
+    print("0. 종료")
+    selection = int(input("기능 선택 >>>>>>> "))
 
 
-        if selection == 0:
-            break
-        elif selection == 1:
-            li = stock_list_by_price(li)
-        elif selection == 2:
-            li = stock_list_by_amount(li)
-        elif selection == 3:
-            li = initialized_li
-            print("초기화가 완료되었습니다.")
-        elif selection == 4:
-            read_list(li)
-        
+    if selection == 0:
+        break
+    elif selection == 1:
+        li = stock_list_by_price(li)
+    elif selection == 2:
+        li = stock_list_by_amount(li)
+    elif selection == 3:
+        li = stock_list_by_purchase(li, 1)
+    elif selection == 4:
+        li = stock_list_by_purchase(li, 2)
+    elif selection == 5:
+        li = initialized_li
+        print("초기화가 완료되었습니다.")
+    elif selection == 6:
+        read_list(li)
     
-except:
-    print("오류가 발생했습니다.")
-finally:
-    print("프로그램을 종료합니다.")
-    driver.quit()
+
+# except:
+#     print("오류가 발생했습니다.")
+# finally:
+#     print("프로그램을 종료합니다.")
+#     driver.quit()
--- project/종목발굴/stock_filter.py
+++ project/종목발굴/stock_filter.py
@@ -1,6 +1,9 @@
 # 1.1.0 : main 함수 내에서 동작하는 메소드로 정의.
 # 1.1.1 : 리스트를 통하여 스크래핑 진행.
 # 1.1.2 : 거래량 기준 필터 기능 추가
+# 1.1.4 : 외인 / 기관 연일 순매수 필터 기능 추가
+import requests
+from bs4 import BeautifulSoup
 
 def finalize(li):
     print("검색을 종료합니다. ", end="")
@@ -36,5 +39,40 @@ def stock_list_by_amount(li):
         elif ifs == 2 and element[3] not in range(minimum_amount, maximum_amount+1):
             continue
         result.append(element)
+    finalize(result)
+    return result
+
+# 1.1.4 : 외인 / 기관 연일 순매수 필터 기능 추가
+def stock_list_by_purchase(li, selection): # selection = 1이면 외인, 2면 기관 매수
+    if selection == 1:
+        col_num = 2
+    else: col_num = 3
+    result = []
+    purchase_days = int(input("최근 몇일 연속 순매수한 종목을 찾고 싶으신지 입력해주세요(최대 6일까지 가능): "))
+    if purchase_days not in range(1,7):
+        print("잘못된 일수를 입력하셨습니다. 함수를 종료합니다.")
+        return
+    
+    for elem in li:
+        res = requests.get(elem[4])
+        print(elem[0])
+        res.raise_for_status()
+        soup = BeautifulSoup(res.text, "lxml")
+
+        table = soup.find("div", attrs={"class":"sub_section right"}).find("table").tbody.find_all("tr")[1:-1]
+        if not table:
+            continue
+        for day in range(purchase_days):
+            try:
+                purchase_amounts = int(table[day].find_all("td")[col_num].get_text().strip().replace(",",""))
+                if purchase_amounts < 0:
+                    break
+            except:
+                source = table[day].find("em", attrs={"class":"sam"})
+                if source:
+                    break
+                day = purchase_days - 1
+            if day == purchase_days - 1:
+                result.append(elem)
     finalize(result)
     return result
