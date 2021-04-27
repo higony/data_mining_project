26b4026496dbc28bc23111549174782229a88fa6
higony <jenissjhk@likelion.org>
2021-04-27 10:21:59 +0900

    stock ver 1.1.6

--- project/종목발굴/main.py
+++ project/종목발굴/main.py
@@ -11,52 +11,52 @@ driver = webdriver.Chrome()
 initialized_li = initialize(driver)
 li = initialized_li
 
-# try:
-while True:
-    print("원하시는 필터 적용 및 기능을 선택해주세요.")
-    print("1. 가격별 필터")
-    # 1.1.2 : 거래량 기준 필터 기능 추가
-    print("2. 거래량 별 필터")
-    # 1.1.4 : 외인 / 기관 연일 순매수 필터 기능 추가
-    print("3. 외인 순매수 별 필터")
-    print("4. 기관 순매수 별 필터")
-    # 1.1.5 : ETF, ETN, 스팩 종목 제외 필터 기능 추가
-    print("5. ETF, ETN, 스팩 종목 제외 필터")
-    # 1.1.6 : 등락율 별 필터 기능 추가
-    print("6. 등락율 별 필터")
-    print("7. 필터 초기화")
-    print("8. 필터링 된 종목 조회")
-    print("0. 종료")
-    selection = int(input("기능 선택 >>>>>>> "))
+try:
+    while True:
+        print("원하시는 필터 적용 및 기능을 선택해주세요.")
+        print("1. 가격별 필터")
+        # 1.1.2 : 거래량 기준 필터 기능 추가
+        print("2. 거래량 별 필터")
+        # 1.1.4 : 외인 / 기관 연일 순매수 필터 기능 추가
+        print("3. 외인 순매수 별 필터")
+        print("4. 기관 순매수 별 필터")
+        # 1.1.5 : ETF, ETN, 스팩 종목 제외 필터 기능 추가
+        print("5. ETF, ETN, 스팩 종목 제외 필터")
+        # 1.1.6 : 등락율 별 필터 기능 추가
+        print("6. 등락율 별 필터")
+        print("7. 필터 초기화")
+        print("8. 필터링 된 종목 조회")
+        print("0. 종료")
+        selection = int(input("기능 선택 >>>>>>> "))
 
 
-    if selection == 0:
-        break
-    elif selection == 1:
-        li = stock_list_by_price(li)
-    elif selection == 2:
-        li = stock_list_by_amount(li)
-    elif selection == 3:
-        li = stock_list_by_purchase(li, 1)
-    elif selection == 4:
-        li = stock_list_by_purchase(li, 2)
-    elif selection == 5:
-        li = stock_list_by_eliminating(li, driver)
-    elif selection == 6:
-        li = stock_list_by_raise(li)
-    elif selection == 7:
-        # 1.1.6 : 실시간 정보로 수정할 수 있게 변경.
-        li = initialize(driver)
-        print("초기화가 완료되었습니다.")
-    elif selection == 8:
-        read_list(li)
+        if selection == 0:
+            break
+        elif selection == 1:
+            li = stock_list_by_price(li)
+        elif selection == 2:
+            li = stock_list_by_amount(li)
+        elif selection == 3:
+            li = stock_list_by_purchase(li, 1)
+        elif selection == 4:
+            li = stock_list_by_purchase(li, 2)
+        elif selection == 5:
+            li = stock_list_by_eliminating(li, driver)
+        elif selection == 6:
+            li = stock_list_by_raise(li)
+        elif selection == 7:
+            # 1.1.6 : 실시간 정보로 수정할 수 있게 변경.
+            li = initialize(driver)
+            print("초기화가 완료되었습니다.")
+        elif selection == 8:
+            read_list(li)
 
 
-# except:
-#     print("오류가 발생했습니다.")
-# finally:
-#     print("프로그램을 종료합니다.")
-#     driver.quit()
+except:
+    print("오류가 발생했습니다.")
+finally:
+    print("프로그램을 종료합니다.")
+    driver.quit()
 
 
 # api 주소 : https://api.finance.naver.com/siseJson.naver?symbol=217820&requestType=1&startTime=20210423&endTime=20210426&timeframe=day
